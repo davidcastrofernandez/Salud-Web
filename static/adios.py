f247e698aa94ad7b659afea8f6abbde8d4fe87d4
davidfernandocastro <osdavidcastrofernandez@gmail.com>
2019-05-17 18:17:26 -0400

    statics included

---
+++ static/adios.py
@@ -0,0 +1,10 @@
+<html>
+    <heand>
+
+    <title>This is website</title>   
+    </head>
+    <body>
+        <h1 style="color:black;">adios</h1>
+        <p>el ejercico se ha terminado</p>
+    </body>
+</html>    
