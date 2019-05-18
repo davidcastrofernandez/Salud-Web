025e1d23b31af31a65158361316071ca53bfc911
mauricio <acostamauricio336699@gmail.com>
2019-05-18 13:31:47 -0400

    prueba app.py

--- app.py
+++ app.py
@@ -1,45 +1,17 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hola_mundo():
-    return   """
-        <h1>hello word</h1>
-        <br />
-        <p> This is Paragraph </p>   
-"""
-@app.route("/hola")
-def render_html():
-    return render_template("index.html")
-
-@app.route("/adios")
-def adios_html():
-    return render_template("index.html")
+database = {
+    "5591945":{"nombre":"Mauricio","apellido": "Acosta","fecha_de_nacimiento":"23-12-2002","sexo":"m","ciudad":"San_Antonio","grupo_sanguineo":"positivo","telefono":"98768785"},
+    "6970882":{"nombre":"laura","apellido": "gomez","fecha_de_nacimiento":"27-04-2002","sexo":"f","ciudad":"Villa_Ygatimi","grupo_sanguineo":"ARH+ +","telefono":"8678858678"},
+    "4238240":{"nombre":"David", "apellido": "Castro","fecha_de_nacimiento":"03-06-1993","sexo":"m","ciudad":"chaco","grupo_sanguineo":"ARH+","telefono":"9878996"}
+}
+#print(database["5591945"]["nombre"])
+@app.route("/datos/<string:cedula>/")
+def datos(cedula):
+    return render_template("datos.html",paciente=database[cedula])
 
 
-@app.route("/usuario")
-def render2_html():
-    personainfo = {
-        "nombre": "Juan",
-        "edad":24
-        }
-    return personainfo["nombre"]
-
-@app.route("/edad")
-def edad():
-    personainfo = {
-        "nombre": "Juan",
-        "edad":24
-        }
-    personainfo["edad"]=personainfo["edad"]+1   
-    return str(personainfo["edad"])
-
-@app.route("/info/<string:campo>/")
+@app.route("/vacunacion/<string:campo>/")
 def nombre(campo):
-    personainfo = {
-        "nombre": "juan",
-        "edad":24,
-        "cedula":3667788,
-        "telefono":982345345
-        }
     return str(personainfo[campo])
