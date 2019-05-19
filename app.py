3ff9f10b3dd59dde721c3814d43a5f0382eb6848
mauricio <acostamauricio336699@gmail.com>
2019-05-19 11:43:45 -0400

    ultima actualizacion

--- app.py
+++ app.py
@@ -1,38 +1,116 @@
 from flask import Flask,render_template,request,redirect
+
+import socket 
 app = Flask(__name__)
 cedula=""
-database = {
-    "5591945":{"nombre":"Mauricio","apellido": "Acosta","fecha_de_nacimiento":"23-12-2002","sexo":"m",
-    "ciudad":"San_Antonio","grupo_sanguineo":"positivo","telefono":"98768785",
+ip='192.168.1.130'
+
+
+
+persona1 ={
+        "5591945": {
+                "nombre":"Mauricio",
+                "apellido": "Acosta",
+                "fecha_de_nacimiento":"23-12-2002",
+                "sexo":"m",
+                "ciudad":"San_Antonio",
+                "grupo_sanguineo":"positivo",
+                "telefono":"98768785",
+                "vacunas":[
+                    {"fecha":"18-06-2019","vacuna_aplicada":"antigripal","proxima_vacuna":"28-10-2020"},
+                    {"fecha":"23-03-2018","vacuna_aplicada":"antirabico","proxima_vacuna":"18-08-2019"},
+                    {"fecha":"13-07-2019","vacuna_aplicada":"antitetanico","proxima vacuna":"20-02-2019"}
+                    ]
+                }, 
+        
+        "982145358":{
+                "nombre":"paula",
+                "apellido": "santacruz",
+                "fecha_de_nacimiento":"24-09-2002",
+                "sexo":"f",
+                "ciudad":"capiata",
+                "grupo_sanguineo":"positivo",
+                "telefono":"765765",
+                "vacunas":[
+                    {"fecha":"10-08-2019","vacuna_aplicada":"antigripal","proxima_vacuna":"10-08-2020"},
+                    {"fecha":"26-12-2019","vacuna_aplicada":"viruela","proxima_vacuna":"28-10-2020"},
+                    {"fecha":"13-02-2019","vacuna_aplicada":"hepatitis","proxima_vacuna":"28-10-2020"}]
+                }
+
+
+}
+               
+"""
+persona = [
+    {
+    "cedula":5591945,
+    "nombre":"Mauricio",
+    "apellido": "Acosta",
+    "fecha_de_nacimiento":"23-12-2002",
+    "sexo":"m",
+    "ciudad":"San_Antonio",
+    "grupo_sanguineo":"positivo",
+    "telefono":"98768785",
     "vacunas":[
         {"fecha":"18-06-2019","vacuna_aplicada":"antigripal","proxima_vacuna":"28-10-2020"},
         {"fecha":"23-03-2018","vacuna_aplicada":"antirabico","proxima_vacuna":"18-08-2019"},
         {"fecha":"13-07-2019","vacuna_aplicada":"antitetanico","proxima vacuna":"20-02-2019"}
         ]
     },
-    "6970882":{"nombre":"laura","apellido": "gomez","fecha_de_nacimiento":"27-04-2002","sexo":"f","ciudad":"Villa_Ygatimi","grupo_sanguineo":"ARH+ +","telefono":"8678858678",
-    "vacunas":[
-        {"fecha":"13-09-2019","vacuna_aplicada":"antigripal","proxima_vacuna":"13-09-2020"},
-        {"fecha":"09-06-2016","vacuna_aplicada":"antitetanos","proxima_vacuna":"06_09-2022"},
-        {"fecha":"18-06-2019","vacuna_aplicada":"culebrilla","proxima_vacuna":"04-10-2020"}
-    ]
-    },
-    "4238240":{"nombre":"David", "apellido": "Castro","fecha_de_nacimiento":"03-06-1993","sexo":"m",
-    "ciudad":"chaco","grupo_sanguineo":"ARH+","telefono":"9878996",
+    {
+    "cedula":982145358,
+    "nombre":"paula",
+    "apellido": "santacruz",
+    "fecha_de_nacimiento":"24-09-2002",
+    "sexo":"f",
+    "ciudad":"capiata",
+    "grupo_sanguineo":"positivo",
+    "telefono":"765765",
     "vacunas":[
         {"fecha":"10-08-2019","vacuna_aplicada":"antigripal","proxima_vacuna":"10-08-2020"},
         {"fecha":"26-12-2019","vacuna_aplicada":"viruela","proxima_vacuna":"28-10-2020"},
-        {"fecha":"13-02-2019","vacuna_aplicada":"hepatitis","proxima_vacuna":"28-10-2020"}
-    ]
+        {"fecha":"13-02-2019","vacuna_aplicada":"hepatitis","proxima_vacuna":"28-10-2020"}]
+    },
+    {
+    "cedula":972543207,
+    "nombre":"laura",
+    "apellido": "gomez",
+    "fecha_de_nacimiento":"27-04-2019",
+    "sexo":"f",
+    "ciudad":"villa ygatimi",
+    "grupo_sanguineo":"0+",
+    "telefono":"765765",
+    "vacunas":[
+        {"fecha":"16-09-2019","vacuna_aplicada":"antigripal","proxima_vacuna":"11-08-2020"},
+        {"fecha":"16-12-2019","vacuna_aplicada":"viruela","proxima_vacuna":"20-10-2020"},
+        {"fecha":"19-02-2019","vacuna_aplicada":"hepatitis","proxima_vacuna":"24-10-2020"}]
+    },
+    {
+    "cedula":98375984,
+    "nombre":"david",
+    "apellido": "castro",
+    "fecha_de_nacimiento":"06-05-1994",
+    "sexo":"m",
+    "ciudad":"Chaco",
+    "grupo_sanguineo":"0+",
+    "telefono":"765765",
+    "vacunas":[
+        {"fecha":"10-08-2019","vacuna_aplicada":"antigripal","proxima_vacuna":"10-08-2020"},
+        {"fecha":"26-12-2019","vacuna_aplicada":"viruela","proxima_vacuna":"28-10-2020"},
+        {"fecha":"13-02-2019","vacuna_aplicada":"hepatitis","proxima_vacuna":"28-10-2020"}]
     }
-}
+
+]  
+
+"""
 #print(database["5591945"]["nombre"])
 @app.route("/datos/<string:cedula>/")
 def datos(cedula):
-    return render_template("datos.html",paciente=database[cedula],cedula=cedula)
+    return render_template("datos.html",paciente=persona1[cedula],cedula=cedula)
 
 @app.route("/",methods=['GET','POST'])
 def for_home():
+    local="http://127.0.0.1:5000/datos/"
     if request.method=='POST':
         cedula=request.form.get('cedula')
         return redirect("http://127.0.0.1:5000/datos/"+cedula)
@@ -40,7 +118,7 @@ def for_home():
 
 @app.route("/vacuna/<string:cedula>/")
 def vacuna_html(cedula):
-    return render_template("vacuna.html",paciente=database[cedula],cedula=cedula)
+    return render_template("vacuna.html",paciente=persona1[cedula],cedula=cedula)
 
 @app.route("/historial")
 def historial_html():
@@ -70,6 +148,15 @@ def for_preguntas():
         diccionario["grupo_sanguineo"]= grupo_sanguineo
         diccionario["telefono"]= telefono
         diccionario["vacunas"]=[]
-        database[cedula]=diccionario
-        return redirect("http://127.0.0.1:5000/datos/"+cedula)
+        persona1[cedula]=diccionario
+       
+        redireccionar="http://"+ip +"/datos/"
+        local="http://127.0.0.1:5000/datos/"
+        print(redireccionar)
+        return redirect(local+cedula)
     return render_template("registro.html")
+
+
+if __name__=='__main__':
+    ip='192.168.1.130'
+    app.run(host=ip)
